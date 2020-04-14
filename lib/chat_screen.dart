c89eed19b284b5f4e29ec7a0cff5c9a0d81bc901
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-14 18:52:53 -0300

    criado parte de enviar mensagem, gravando na collection do firestore

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_online/text_compose.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,6 +8,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +16,16 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("Olá"),
         elevation: 0,
       ),
-      body: TextCompose(),
+      body: TextCompose(_sendMessage),
     );
   }
+
+  _sendMessage(String text) {
+    Firestore.instance.collection("messages").add(
+      {
+        'text': text,
+      }
+    );
+  }
+
 }
--- lib/text_compose.dart
+++ lib/text_compose.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 class TextCompose extends StatefulWidget {
+  final Function(String) sendMessage;
+
+  TextCompose(this.sendMessage);
+
   @override
   _TextComposeState createState() => _TextComposeState();
 }
 
 class _TextComposeState extends State<TextCompose> {
-
   bool _isComposing = false;
+
+  final TextEditingController _textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,18 +23,34 @@ class _TextComposeState extends State<TextCompose> {
           IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
           Expanded(
             child: TextField(
-              decoration: InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
+              controller: _textController,
+              decoration:
+                  InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
               onChanged: (text) {
                 setState(() {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text) {},
+              onSubmitted: (text) {
+                _sendMessage(text);
+              },
             ),
           ),
-          IconButton(icon: Icon(Icons.send), onPressed: _isComposing ? () {} : null)
+          IconButton(
+              icon: Icon(Icons.send),
+              onPressed: _isComposing
+                  ? () => _sendMessage(_textController.text)
+                  : null)
         ],
       ),
     );
+  }
+
+  void _sendMessage(String text) {
+    widget.sendMessage(text);
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
   }
 }
